d6e5ed8966f6dbfd576853a793eb556fed3c4dad
n2khanh <n2.khanh.git@gmail.com>
2024-10-22 19:02:20 +0700

    begin

---
+++ Zoo_template/Zoo_template/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Zoo_template.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
